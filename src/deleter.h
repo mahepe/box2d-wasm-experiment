259823c4ced6772c290b142eee1d7fbfd705b7d3
Matias Heikkilä <matias.heikkila@futurice.com>
2019-03-01 13:31:32 +0200

    Moving around + formatting

--- src/deleter.h
+++ src/deleter.h
@@ -3,19 +3,13 @@
 
 // This interfaces the smart pointers with SDL's destruction methods.
 struct Deleter {
-  inline void operator()(SDL_Window* window) {
-    SDL_DestroyWindow(window);
-  }
+  inline void operator()(SDL_Window *window) { SDL_DestroyWindow(window); }
 
-  inline void operator()(SDL_Texture* texture) {
-    SDL_DestroyTexture(texture);
-  }
+  inline void operator()(SDL_Texture *texture) { SDL_DestroyTexture(texture); }
 
-  inline void operator()(SDL_Renderer* renderer) {
+  inline void operator()(SDL_Renderer *renderer) {
     SDL_DestroyRenderer(renderer);
   }
 
-  inline void operator()(b2Body* body){
-    body -> GetWorld() -> DestroyBody(body);
-  }
+  inline void operator()(b2Body *body) { body->GetWorld()->DestroyBody(body); }
 };
