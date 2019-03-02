1972f6d55b9f3f933eabee61dd087e534ad48ee5
Matias Heikkilä <matias.heikkila@futurice.com>
2019-03-02 13:59:37 +0200

    asd

--- src/const.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-#define SCREEN_WIDTH 800
-#define SCREEN_HEIGHT 600
-#define PX_PER_UNIT 24
--- src/game_state.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "game_object.h"
-
-struct GameState {
-public:
-  std::vector<std::unique_ptr<GameObject>> objects;
-  std::unique_ptr<b2Body, Deleter> camera;
-};
